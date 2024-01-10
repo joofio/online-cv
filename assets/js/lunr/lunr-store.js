9612b67a2c5abedc6fbfd618de0546405cd0c614
joofio <joofio@users.noreply.github.com>
2024-01-10 10:04:37 +0000

    jekyll build from Action 216791acfabefab30a74e578c44d4a48c539a1fd

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Introduction to Machine Learning with Julia",
+        "excerpt":"The Julia language was originally released in 2012 by Alan Edelman, Stefan Karpinski, Jeff Bezanson, and Viral Shah. Its popularity has been increasing exponentially in the last few years and its speed and community have been key. Furthermore, and taking the words of Ben Lauwens in its book Think Julia...","categories": ["machine-learning","julia"],
+        "tags": [],
+        "url": "/posts/2020-10-15-Classification-julia.html",
+        "teaser": null
+      },{
+        "title": "Regression Tutorial with Julia lang",
+        "excerpt":"Hi, in my last post, I showed how Julia can be used to perform a classification task. In that case, we classified patients into two categories, so it was a classification, which is a method for predicting a, you guessed it, categorical outcome. But now, we are going to do...","categories": ["machine-learning","julia","regression"],
+        "tags": [],
+        "url": "/posts/2021-12-15-regression-julia.html",
+        "teaser": null
+      },{
+        "title": "Academic Resources",
+        "excerpt":"Grad students are often confronted with a great task in order to finish their thesis. From writing our thesis, keeping up with the state-of-the-art of our areas. Publishing quality content, with text, images and tables with industry-grade quality while managing to balance all of this into tight schedules is hard...","categories": ["academic","resources","lists","phD"],
+        "tags": [],
+        "url": "/posts/2022-08-16-Academic-resources.html",
+        "teaser": null
+      },{
+        "title": "Tech Tips #0",
+        "excerpt":"I’m launching a new category dedicated to small tech tips, where I’ll document the challenges and solutions I encounter during my PhD journey, as well as in my broader research and technology-related endeavors. To kick things off, here’s a handy tip: Converting Bayesian networks from pgmpy to bnlearn and then...","categories": ["tech","tutorial","bayes","phD"],
+        "tags": [],
+        "url": "/posts/2023-01-10-Converting-bif-to-net.html",
+        "teaser": null
+      }]
